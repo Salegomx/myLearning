d8751bb8a5cdb7e845305f02d243c40ad6b23a27
Sergio <sergioagmoreno@gmail.com>
2023-10-21 21:13:38 -0500

    Cree un HTML para unos apuntes de JS e hice ejercicios de operadores

--- js/index.js
+++ js/index.js
@@ -133,3 +133,41 @@ else {
     console.log("Los numeros son IGUALES en valor (sin considerar el tipo de dato)");
 } */
 
+// Ejercicio con un if short
+
+/* let amigo = false;
+
+let amigos = [
+    "Pedro", 
+    "Juan", 
+    "Pablo", 
+    "Arturo",
+    amigo ? "Carlos" : "Pedro"
+];
+
+console.log(amigos); */
+
+// Sintaxis de if else usando la tecla de tabulador
+
+/* let efectivo = 500;
+let carrito = 300;
+
+if (efectivo > carrito)
+    console.log("Puedes pagar");
+else
+    console.log("Te falta dinero"); */
+
+// Ejercicio de if short anidados
+
+/* let calificacion = 68;
+
+let grado = calificacion >= 95 ? "A+" :
+    calificacion >= 90 ? "A" :
+    calificacion >= 85 ? "B+" :
+    calificacion >= 80 ? "B" :
+    calificacion >= 75 ? "C+" :
+    calificacion >= 70 ? "C" :
+    calificacion >= 65 ? "D+" :
+    calificacion >= 60 ? "D" : "F";
+
+    console.log(calificacion, grado); */
