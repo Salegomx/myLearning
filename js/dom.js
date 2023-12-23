b3717b86d8cd9f86d4e583693d5f17811cc3bb70
Sergio <sergioagmoreno@gmail.com>
2023-12-22 20:30:54 -0500

    Agregue mas ejemplos de dom

--- js/dom.js
+++ js/dom.js
@@ -13,3 +13,17 @@ nuevaVentana.open();*/
 // console.log(document); - Es igual que poner window.document
 
 // console.log(document.documentElement) - Es igual a document y window.document
+
+// let doc = document.documentElement; Asignando un elemento a una variable
+
+// let doc = document.documentElement;
+
+ // console.log(doc.outerHTML) outherHTML contiene todo el archivo HTML que muestra el navegador
+
+ // console.log(document.head); Puedes seleccionar partes especificas del HTML
+
+ // console.log(document.body); Puedes seleccionar directamente el body
+
+ // console.log(document.doctype); Muestra el tipo de documento con el que estas trabajando
+
+ // console.log(document.charset); Muestra el tipo de caracteres que puedes usar que por default es UTF-8
