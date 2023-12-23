ec153aa40c2e386b59cb36f04f250f6c851e868d
Sergio <sergioagmoreno@gmail.com>
2023-12-22 20:16:34 -0500

    Añadi ejemplos de DOM al archivo dom.js

--- js/dom.js
+++ js/dom.js
@@ -5,3 +5,11 @@
 /*var nuevaVentana = window.open("https://www.google.com", "nuevaVentana", "width=400, heigth=300");
 
 nuevaVentana.open();*/
+
+// console.log(window.document); - Imprime el documento en la consola
+
+// console.log(window); - Imprime el objeto window que contiene todas las funciones de JS
+
+// console.log(document); - Es igual que poner window.document
+
+// console.log(document.documentElement) - Es igual a document y window.document
