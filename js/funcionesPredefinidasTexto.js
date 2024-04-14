6af28bb9f26b899ae33d0da41ed32bad9bd1e561
Sergio <sergioagmoreno@gmail.com>
2024-04-14 14:59:40 -0500

    Hice mas ejercicios

---
+++ js/funcionesPredefinidasTexto.js
@@ -0,0 +1,45 @@
+/*let saludo = "Hola!";
+let nombre = "Sergio";
+let mensaje = saludo.concat("----", nombre);
+
+console.log(mensaje);*/
+
+/*let texto = "Hola Mundo";
+console.log(texto.toLowerCase());*/
+
+/*let texto = "Hola Mundo";
+console.log(texto.toUpperCase());*/
+
+/*let texto = "      Hola Mundo     ";
+console.log(texto.trim());*/
+
+/*let texto = "Javascript";
+console.log(texto.substring(4, 10));*/
+
+/*let texto = "Hola como estas?";
+let palabra = texto.split(" ");
+console.log(palabra[2]);*/
+
+/*let texto = "Hola, hola mundo, juanito";
+console.log(texto.indexOf("juanito"));*/
+
+/*let texto = "Hola, hola mundo, hola juanito, hola";
+console.log(texto.lastIndexOf("hola"));*/
+
+/*let texto = "  hola como estas?      ";
+let palabras = texto.length;
+console.log(texto);
+console.log(palabras);
+console.log(texto.trimStart());
+console.log(palabras);*/
+
+/*let texto = "  hola como estas?      ";
+let palabras = texto.length;
+console.log(texto);
+console.log(palabras);
+console.log(texto.trimEnd());
+console.log(palabras);*/
+
+/*let texto = "      Hola como estas?       ";
+console.log(texto);
+console.log(texto.trimStart().trimEnd());*/
--- js/tiposDeOperadores.js
+++ js/tiposDeOperadores.js
@@ -8,14 +8,14 @@
 // 3. Multiplicacion y division
 // 4. Suma y resta
 
-console.log(5 + 5); // 10
+/*console.log(5 + 5); // 10
 console.log(5 + 5 - 10); // 0
 console.log(5 + (5 - 10)); // 0
 console.log(5 - (10 + 15 - 20) - 8); // -8
 console.log(3 - 5 + (18 - 28 + 36) - (-36 + 48 - 56)); // -68
 console.log(7 + 8 - 26 - (-36 + 45 - 56) - (-80 + 50)); // 66
 console.log((-20 + 16) - 4 + (-40 - 26) - (-36 - 48 - 50)); // 60
-console.log(20 + 100 -35 + (-36 + 8 + 4) - (-24 + 12) + (-12 - 24)); // 37
+console.log(20 + 100 -35 + (-36 + 8 + 4) - (-24 + 12) + (-12 - 24)); // 37*/
 
 // Operadores relacionales
 
@@ -23,7 +23,7 @@ console.log(20 + 100 -35 + (-36 + 8 + 4) - (-24 + 12) + (-12 - 24)); // 37
 // Diferencia entre = (asignacion), == (evalua el valor) y === (evalua el valor y el tipo de dato). 
 // TIP: la buena practica siempre debes usar el ===
 
-console.log(8 > 10); // false
+/*console.log(8 > 10); // false
 console.log(9 > 8); // true
 console.log(9 >= 8); // true
 console.log(7 < 7); // false
@@ -40,4 +40,10 @@ console.log('2' > {}); // false JS intenta convertir el objeto a un dato primiti
 console.log('2' > NaN); // false No hay forma de hacer una comparación
 console.log('2' > false); // true
 console.log('2' > true); // true
-console.log('0' > true); // false
+console.log('0' > true); // false*/
+
+let i = 5;
+console.log(i += 10);
+
+let j = 20;
+console.log(j -= 10);
