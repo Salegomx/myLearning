63af0e0a47170d95c87f73d6a69cc623684993a1
Sergio <sergioagmoreno@gmail.com>
2024-04-18 11:09:46 -0500

    Anadi nuevos ejercicios

--- js/funcionesPredefinidasTexto.js
+++ js/funcionesPredefinidasTexto.js
@@ -43,3 +43,60 @@ console.log(palabras);*/
 /*let texto = "      Hola como estas?       ";
 console.log(texto);
 console.log(texto.trimStart().trimEnd());*/
+
+/*let texto = "Hola mundo";
+console.log(texto.replace("Hola", "Hello"));*/
+
+/*let texto = "Hola como estas?";
+console.log(texto.slice(2)); // la como estas? */
+
+/*let texto = "Hola como estas?";
+console.log(texto.charAt(0)) // H */
+
+/*let frase = "Monitor de 20 pulgadas";
+let frase2 = "OFERTA!!! ".repeat(3);
+console.log(`${frase} ${frase2}`); // Monitor de 20 pulgadas OFERTA!!! OFERTA!!! OFERTA!!! */
+
+/*let numero = 8;
+let convertir = numero.toString();
+console.log(typeof convertir); // string */
+
+/*let resultado = Math.PI;
+console.log(resultado);*/
+
+/*let resultado = Math.round(8.49);
+console.log(resultado); // 8 */
+
+/*let numero = Math.ceil(8.000001);
+console.log(numero); // 9 */
+
+/*let numero = Math.floor(8.99999);
+console.log(numero); // 8 */
+
+/*let raiz = Math.sqrt(81);
+console.log(raiz) // 9 */
+
+/*let absoluto = Math.abs(-5);
+console.log(absoluto); // 5 */
+
+/*let potencia = Math.pow(4, 3);
+console.log(potencia); // 64 */
+
+/*let minimo = Math.min(5, 15, 42, -7, 82, -123);
+console.log(minimo); // -123 */
+
+/*let maximo = Math.max(5, 15, 42, -7, 82, -123)
+console.log(maximo); // 82 */
+
+/*let aleatorio = Math.random() * 50; // Genera un numero entre 0.01 y 0.99
+let redondeo = Math.floor(aleatorio); // redeondeo hacia abajo
+console.log(redondeo); // numero entero aleatorio */
+
+/*let numero = 15;
+console.log(numero++);
+console.log(numero);
+console.log(++numero);*/
+
+/*for(let i = 0; i <= 100; i += 5) {
+    console.log(i);
+}*/
